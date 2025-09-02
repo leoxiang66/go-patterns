a56344f83d915810aec37d1630387fe75e3e6a96
Leo Xiang <leoxiang66@users.noreply.github.com>
2025-09-02 10:49:01 +0800

    v0.6.0
    static rate limiter

---
+++ parallel/race/FixedLimiter.go
@@ -0,0 +1,35 @@
+package race
+
+import (
+	"sync"
+	"time"
+)
+
+type StaticLimiter struct {
+	ticker *time.Ticker
+	mu     sync.Mutex
+}
+
+func NewStaticLimiter(interval time.Duration) *StaticLimiter {
+	return &StaticLimiter{
+		ticker: time.NewTicker(interval),
+	}
+}
+
+func (l *StaticLimiter) GrantNextToken() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	<-l.ticker.C
+}
+
+func (l *StaticLimiter) Reset(interval time.Duration) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.ticker.Reset(interval)
+}
+
+func (l *StaticLimiter) Stop() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.ticker.Stop()
+}
